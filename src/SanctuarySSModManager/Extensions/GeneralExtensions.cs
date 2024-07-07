fd0ed221a43cf0a5778150c179c4fceb3bb5032c
Jon LeVee <jonlevee@gmail.com>
2024-07-06 22:49:43 -0700

    Added snapshot

---
+++ src/SanctuarySSModManager/Extensions/GeneralExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanctuarySSModManager.Extensions
+{
+    internal static class GeneralExtensions
+    {
+        public static void EnsureDirectoryExists(this string directory)
+        {
+            var info = new DirectoryInfo(directory);
+            if (info.Exists)
+            {
+                return;
+            }
+            if (info.FullName.Equals(info.Root.FullName, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new DirectoryNotFoundException($"Directory root {info.Root.FullName} does not exist");
+            }
+            EnsureDirectoryExists(info.Parent.FullName);
+            Directory.CreateDirectory(info.FullName);
+        }
+    }
+}
---
+++ src/SanctuarySSModManager/ModManagerMetaData.cs
@@ -0,0 +1,62 @@
+﻿using SanctuarySSModManager.Extensions;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SanctuarySSModManager
+{
+    public class ModManagerMetaData
+    {
+        private static JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        };
+
+        private static ModManagerMetaData? instance;
+        public static ModManagerMetaData Instance => instance ??= LoadSettings();
+
+        private static ModManagerMetaData LoadSettings()
+        {
+            ModManagerMetaData settings = new ModManagerMetaData();
+            var appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appName = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyProductAttribute>()?.Product;
+            if (string.IsNullOrWhiteSpace(appName))
+            {
+                throw new ApplicationException("Could not locate AssemblyProductAttribute");
+            }
+            var appFolder = Path.Combine(appDataFolder, appName);
+            appFolder.EnsureDirectoryExists();
+            var settingsFile = Path.Combine(appFolder, "ModManagerMetaData.json");
+            if (!File.Exists(settingsFile))
+            {
+                var steamInfo = new SteamInfo();
+                settings.ShatteredSunDirectoryRoot = steamInfo.GetRoot("Sanctuary Shattered Sun Demo");
+                settings.ModRootFolder = @"prototype\RuntimeContent\Lua";
+                settings.ModManagerFolder = appFolder;
+
+                var json = JsonSerializer.Serialize(settings, serializerOptions);
+                File.WriteAllText(settingsFile, json);
+            } else
+            {
+                string json = File.ReadAllText(settingsFile);
+                settings = JsonSerializer.Deserialize<ModManagerMetaData>(json) ?? settings;
+            }
+            return settings;
+        }
+
+        public ModManagerMetaData()
+        {
+            ShatteredSunDirectoryRoot = string.Empty;
+            ModRootFolder = string.Empty;
+            ModManagerFolder = string.Empty;
+        }
+
+        public string ShatteredSunDirectoryRoot {  get; set; }
+        public string ModRootFolder { get; set; }
+
+        public string ModManagerFolder { get; set; }
+
+        public string FullModRootFolder => Path.Combine(ShatteredSunDirectoryRoot, ModRootFolder);
+    }
+}
